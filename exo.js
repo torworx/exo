3d9084f5681423b4678c246eb5b2926dcc58432d
taoyuan <taoyuan@evymind.com>
2013-05-28 17:25:43 +0800

    Initial commit.

--- exo.js
+++ exo.js
@@ -285,13 +285,13 @@
      *          };
      *      });
      *
-     * @param {String} className The class name to create in string dot-namespaced format, for example:
+     * @param {String|Object|Function|null} className The class name to create in string dot-namespaced format, for example:
      * 'My.very.awesome.Class', 'FeedViewer.plugin.CoolPager'
      * It is highly recommended to follow this simple convention:
      *  - The root and the class name are 'CamelCased'
      *  - Everything else is lower-cased
      * Pass `null` to create an anonymous class.
-     * @param {Object} data The key - value pairs of properties to apply to this class. Property names can be of any valid
+     * @param {Object|Function|?} data The key - value pairs of properties to apply to this class. Property names can be of any valid
      * strings, except those in the reserved listed below:
      *  - `extend`
      *  - `mixins`
@@ -299,8 +299,6 @@
      *  - `singleton`
      *  - `statics`
      *
-     * @param {Function} createdFn Optional callback to execute after the class is created, the execution scope of which
-     * (`this`) will be the newly created class itself.
      * @return {Object}
      * @member exo
      */
--- test/oop.test.js
+++ test/oop.test.js
@@ -15,10 +15,10 @@ describe("oop testing", function() {
         });
 
         var a = new A();
-        should.equal('A', a.$classname);
-        should.equal(a.f, 1);
-        should.equal(a.f2, 2);
-        should.equal(a.m("s"), "s");
+        a.$classname.should.equal('A');
+        a.f.should.equal(1);
+        a.f2.should.equal(2);
+        a.m("s").should.equal("s");
     });
 
     it('define with extend', function(){
@@ -58,29 +58,29 @@ describe("oop testing", function() {
 
         var p1 = new OvyPerson("Torry");
         p1.setAddress("CN", "BJ", "XY");
-        should.equal('Torry', p1.name);
-        should.equal('CN', p1.country);
-        should.equal('BJ', p1.city);
-        should.equal('XY', p1.street);
+        p1.name.should.equal('Torry');
+        p1.country.should.equal('CN');
+        p1.city.should.equal('BJ');
+        p1.street.should.equal('XY');
 
         var p2 = new OvyChinaGuy("Leo");
         p2.setAddress("BJ", "XY");
-        should.equal('Leo', p2.name);
-        should.equal('China', p2.country);
-        should.equal('BJ', p2.city);
-        should.equal('XY', p2.street);
+        p2.name.should.equal('Leo', p2.name);
+        p2.country.should.equal('China');
+        p2.city.should.equal('BJ');
+        p2.street.should.equal('XY');
 
         var p3 = new OvyBeijingLover("Mary");
         p3.setAddress("XY");
-        should.equal('Mary', p3.name);
-        should.equal('China', p3.country);
-        should.equal('Beijing', p3.city);
-        should.equal('XY', p3.street);
+        p3.name.should.equal('Mary');
+        p3.country.should.equal('China');
+        p3.city.should.equal('Beijing');
+        p3.street.should.equal('XY');
 
         var instanceofTest = p3 instanceof OvyBeijingLover &&
             p3 instanceof OvyChinaGuy &&
             p3 instanceof OvyPerson;
-        should.ok(instanceofTest, 'failed the `instanceof` test.');
+        instanceofTest.should.ok;
     });
 
     it('define with statics', function(){
@@ -92,8 +92,8 @@ describe("oop testing", function() {
                 }
             }
         });
-        should.equal(1, A.f);
-        should.equal('Hello World', A.echo('Hello World'));
+        A.f.should.equal(1);
+        A.echo('Hello World').should.equal('Hello World');
     });
 
     it('define as singleton', function(){
@@ -101,7 +101,7 @@ describe("oop testing", function() {
             singleton: true,
             f: 1
         });
-        should.equal(1, A.f);
+        A.f.should.equal(1);
     });
 
     it('define with private', function(){
@@ -173,11 +173,11 @@ describe("oop testing", function() {
         var bar = new Bar("Bar");
         bar.setOptions("nothing");
 
-        should.equal(bar.name, "Bar", "Invalid extend behavior, constructor must be bound correctly");
-        should.equal(bar.opts, "nothing", "Invalid mixins behavior, constructor must be bound correctly");
-        should.equal(bar.config, "nothing", "Invalid mixins behavior, constructor must be bound correctly");
-        should.ok(bar.bind(), "Invalid mixins behavior");
-        should.equal( !bar.unbind(), true, "Invalid mixins behavior");
+        bar.name.should.equal("Bar", "Invalid extend behavior, constructor must be bound correctly");
+        bar.opts.should.equal("nothing", "Invalid mixins behavior, constructor must be bound correctly");
+        bar.config.should.equal("nothing", "Invalid mixins behavior, constructor must be bound correctly");
+        bar.bind().should.be.ok;
+        bar.unbind().should.not.be.ok;
     });
 
     it('define with inherits', function() {
@@ -211,10 +211,10 @@ describe("oop testing", function() {
         var bar = new Bar("Bar");
         bar.setOptions("nothing");
 
-        should.equal(bar.name, "Bar", "Invalid extend behavior, constructor must be bound correctly");
-        should.equal(bar.opts, "nothing", "Invalid mixins behavior, constructor must be bound correctly");
-        should.ok(bar.bind(), "Invalid mixins behavior");
-        should.equal( !bar.unbind(), true, "Invalid mixins behavior");
+        bar.name.should.equal("Bar", "Invalid extend behavior, constructor must be bound correctly");
+        bar.opts.should.equal("nothing", "Invalid mixins behavior, constructor must be bound correctly");
+        bar.bind().should.be.ok;
+        bar.unbind().should.not.be.ok;
     });
 
     it("mixins classes to a plain Class with exo.mixin", function(){
@@ -255,11 +255,11 @@ describe("oop testing", function() {
         var bar = new Bar("Bar");
         bar.setOptions("nothing");
 
-        should.equal(bar.name, "Bar", "Invalid extend behavior, constructor must be bound correctly");
-        should.equal(bar.opts, "nothing", "Invalid mixins behavior, constructor must be bound correctly");
-        should.equal(bar.config, "nothing", "Invalid mixins behavior, constructor must be bound correctly");
-        should.ok(bar.bind(), "Invalid mixins behavior");
-        should.equal( !bar.unbind(), true, "Invalid mixins behavior");
+        bar.name.should.equal("Bar", "Invalid extend behavior, constructor must be bound correctly");
+        bar.opts.should.equal("nothing", "Invalid mixins behavior, constructor must be bound correctly");
+        bar.config.should.equal("nothing", "Invalid mixins behavior, constructor must be bound correctly");
+        bar.bind().should.be.ok;
+        bar.unbind().should.not.be.ok;
     });
 
 
@@ -294,10 +294,10 @@ describe("oop testing", function() {
         var bar = new Bar("Bar");
         bar.setOptions("nothing");
 
-        should.equal(bar.name, "Bar", "Invalid extend behavior, constructor must be bound correctly");
-        should.equal(bar.opts, "nothing", "Invalid mixins behavior, constructor must be bound correctly");
-        should.ok(bar.bind(), "Invalid mixins behavior");
-        should.equal( !bar.unbind(), true, "Invalid mixins behavior");
+        bar.name.should.equal("Bar", "Invalid extend behavior, constructor must be bound correctly");
+        bar.opts.should.equal("nothing", "Invalid mixins behavior, constructor must be bound correctly");
+        bar.bind().should.be.ok;
+        bar.unbind().should.not.be.ok;
     });
 
     it("object property", function(){
@@ -309,12 +309,12 @@ describe("oop testing", function() {
         });
         var p1 = new Person();
         p1.addresses['home'] = 'home';
-        should.ok(p1.addresses['home']);
+        p1.addresses['home'].should.be.ok;
         var p2 = new Person();
         p2.addresses['company'] = 'company';
-        should.notEqual(p2.addresses, p1.addresses);
-        should.ok(!p2.addresses['home']);
-        should.ok(p2.addresses['company']);
+        p1.addresses.should.not.eql(p2.addresses);
+        should.not.exist(p2.addresses['home']);
+        p2.addresses['company'].should.be.ok;
     });
 
 
@@ -330,7 +330,7 @@ describe("oop testing", function() {
             }
         });
 
-        should.equal((new BeijingPerson).name, 'Beijing');
+        (new BeijingPerson).name.should.equal('Beijing');
     });
 
     it("define subclass without constructor", function() {
@@ -346,7 +346,7 @@ describe("oop testing", function() {
         });
 
         var p = new BeijingPerson('Beijing');
-        should.equal(p.name, 'Beijing');
+        p.name.should.equal('Beijing');
     });
 
     it("exo closure", function() {
@@ -384,9 +384,9 @@ describe("oop testing", function() {
         var p = new OvyBeijingLover2("Mary");
         p.setAddress("CH");
 
-        should.equal(p.name, "Mary");
-        should.equal(p.country, "China");
-        should.equal(p.city, "Beijing");
-        should.equal(p.street, "CH");
+        p.name.should.equal("Mary");
+        p.country.should.equal("China");
+        p.city.should.equal("Beijing");
+        p.street.should.equal("CH");
     });
 });
