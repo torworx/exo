2b74a9ae5c09a12e872570a853642f18fa2dcd15
taoyuan <torworx@gmail.com>
2013-07-10 18:15:06 +0800

    v0.0.4

--- exo.js
+++ exo.js
@@ -93,7 +93,7 @@ exo.applyIf = function (object, config, filter) {
 
 function createProxy(fn, context) {
     return function () {
-        fn.apply(context, arguments);
+        return fn.apply(context, arguments);
     };
 }
 
